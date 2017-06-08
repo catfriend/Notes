2aa744474037d5804eddf400e2cec8c9e0cb29f4
catfriend <eakari@live.com>
2017-06-08 13:26:35 -0700

    set up editor inputs

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Session } from 'meteor/session';
+import { Meteor } from 'meteor/meteor';
 
 import { Notes } from '../api/notes';
 
 export class Editor extends React.Component {
-  render () {
+  handleBodyChange(e) {
+    this.props.call('notes.update', this.props.note._id, {
+      body: e.target.value
+    });
+  }
+  handleTitleChange(e) {
+    this.props.call('notes.update', this.props.note._id, {
+      title: e.target.value
+    });
+  }
+  render() {
     if (this.props.note) {
       return (
-        <p>Here's your note.</p>
+        <div>
+          <input value={this.props.note.title} placeholder="Note Title" onChange={this.handleTitleChange.bind(this)}/>
+          <textarea value={this.props.note.body} placeholder="Type your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+          <button>Delete Note</button>
+        </div>
       );
-
-    }  else {
+    } else {
       return (
         <p>
-          { this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.' }
+          { this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}
         </p>
       );
     }
@@ -31,6 +45,7 @@ export default createContainer(() => {
 
   return {
     selectedNoteId,
-    note: Notes.findOne(selectedNoteId)
+    note: Notes.findOne(selectedNoteId),
+    call: Meteor.call
   };
 }, Editor);
